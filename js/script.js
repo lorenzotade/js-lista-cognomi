c70ecd651612cf8a9f27ee04502489d3bc5b39c8
lorenzotade <lorenzo.tade@gmail.com>
2021-04-12 21:48:56 +0200

    corretto

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 var listaCognomi = ["Marzullo", "Banca", "Bianchi", "Rossi", "Giubertoni", "Volpi", "Gialli", "Neri", "Medici"];
 
 // chiedo all'utente il suo cognome e lo pusho nel mio array
-listaCognomi.push(prompt("Inserisci il tuo cognome:"));
+var congomeUtente = prompt("Inserisci il tuo cognome:");
+listaCognomi.push(congomeUtente);
 
 // ordino l'array
 var listaCognomi = listaCognomi.sort();
@@ -14,8 +15,14 @@ for (var i = 0; i < listaCognomi.length; i++) {
   //salvo l'innerHTML della lista in una variabile
   var elementoPrec = document.getElementById("lista").innerHTML;
 
-  //punto elementoPrec a cui sommo se stesso + l'indice + l'elemento della listaCognomi
-  document.getElementById("lista").innerHTML = elementoPrec +  "<li> N. " + (i+1) + ": " + cognome + "</li>"
+  //punto elementoPrec a cui sommo se stesso + l'indice + cognome inserito dall'utente
+  if (cognome === congomeUtente) {
+    document.getElementById("lista").innerHTML = elementoPrec +  "<li> N. " + (i+1) + ": " + cognome + "</li>"
+    // per tutti gli altri cognomi stampo solo il cognome
+  } else {
+    document.getElementById("lista").innerHTML = elementoPrec +  "<li>" + cognome + "</li>";
+  }
+  
 }
   
 //console.log("Cognome numero " + (i+1) + ": " + listaCognomi[i]);
