1319da0f1dc626c3127f095a9da5c091a5aa0921
lorenzotade <lorenzo.tade@gmail.com>
2021-04-12 16:19:43 +0200

    terminato

--- js/script.js
+++ js/script.js
@@ -1,30 +1,20 @@
 // creo un array di cognomi e lo popolo;
-var listaCognomi = ["Marzullo","Banca", "Bianchi", "Rossi", "Giubertoni", "Volpi", "Gialli", "Neri", "Med"];
+var listaCognomi = ["Marzullo", "Banca", "Bianchi", "Rossi", "Giubertoni", "Volpi", "Gialli", "Neri", "Medici"];
 
 // chiedo all'utente il suo cognome e lo pusho nel mio array
-//listaCognomi.push(prompt("Inserisci il tuo cognome:"));
+listaCognomi.push(prompt("Inserisci il tuo cognome:"));
 
-//console.log(listaCognomi.sort())
+// salvo l'array ordinato in un nuovo array
+var listaCognomi = listaCognomi.sort();
 
-// con un primo ciclo for scorro uno per uno gli elementi della mia lista
 for (var i = 0; i < listaCognomi.length; i++) {
-  // con un secondo ciclo for innestato, per ogni iterazione lui scorre di nuovo all'interno della mia lista
-  for (var j = 0; j < listaCognomi.length; j++) {
-    // con un condizionale if controllo se il primo elemento è maggiore (quindi con una lettera più avanti nell'alfabeto)
-    if (listaCognomi[j] > listaCognomi[j+1]) {
-      // se è così, creo una variabile swap nella quale salvo temporaneamente il valore del primo elemento
-      var swap = listaCognomi[j]
-      // sposto il secondo elemento al posto del primo
-      listaCognomi[j] = listaCognomi[j+1];
-      // salvo il valore di swap (ex primo elemento) nel secondo elemento
-      listaCognomi[j+1] = swap;
-    }
-  }
+  var cognome = listaCognomi[i];
+
   //salvo l'innerHTML della lista in una variabile
   var elementoPrec = document.getElementById("lista").innerHTML;
 
   //punto elementoPrec a cui sommo se stesso + l'indice + l'elemento della listaCognomi
-  document.getElementById("lista").innerHTML = elementoPrec +  "<li> Cognome numero " + (i+1) + ": " + listaCognomi[i] + "</li>"
-  
-  //console.log("Cognome numero " + (i+1) + ": " + listaCognomi[i]);
+  document.getElementById("lista").innerHTML = elementoPrec +  "<li> N. " + (i+1) + ": " + cognome + "</li>"
 }
+  
+//console.log("Cognome numero " + (i+1) + ": " + listaCognomi[i]);
--- script2.js
+++ script2.js
@@ -1,23 +1,18 @@
-var a = 1;
 
-var b = 2; 
+/* BUBBLE SORT */
 
-console.log("A è uguale: "+a)
-console.log("B è uguale: "+b)
-
-var c = 0;
-
-c = a; // ora c = 1
-
-a = b; // ora a è uguale a 2
-
-b = c; // ora b assume il valore di c che è quello di a
-
-console.log("A è uguale: "+a)
-console.log("B è uguale: "+b)
-
-a = b;
-b = a;
-
-console.log("A è uguale: "+a)
-console.log("B è uguale: "+b)
+// con un primo ciclo for scorro uno per uno gli elementi della mia lista
+for (var i = 0; i < listaCognomi.length; i++) {
+  // con un secondo ciclo for innestato, per ogni iterazione lui scorre di nuovo all'interno della mia lista
+  for (var j = 0; j < listaCognomi.length; j++) {
+    // con un condizionale if controllo se il primo elemento è maggiore (quindi con una lettera più avanti nell'alfabeto)
+    if (listaCognomi[j] > listaCognomi[j+1]) {
+      // se è così, creo una variabile swap nella quale salvo temporaneamente il valore del primo elemento
+      var swap = listaCognomi[j]
+      // sposto il secondo elemento al posto del primo
+      listaCognomi[j] = listaCognomi[j+1];
+      // salvo il valore di swap (ex primo elemento) nel secondo elemento
+      listaCognomi[j+1] = swap;
+    }
+  }
+}
